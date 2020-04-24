c4c268165fc5bbcbfeecf0dd5126e1504c2b4937
18004885 <18004885@myrp.edu.sg>
2020-04-24 11:55:46 +0800

    FINAL

--- app/src/main/java/sg/edu/rp/c346/demoactivities/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/demoactivities/MainActivity.java
@@ -10,13 +10,27 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    EditText etName, etAge;
+    Button btnDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etName = findViewById(R.id.etName);
+        etAge = findViewById(R.id.etAge);
+        btnDone = findViewById(R.id.btnDone);
 
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] info = {etName.getText().toString(),etAge.getText().toString()};
+
+                Intent i = new Intent(MainActivity.this,DemoActivites2.class);
+                i.putExtra("info",info);
+                startActivity(i);
+            }
+        });
 
 
     }
