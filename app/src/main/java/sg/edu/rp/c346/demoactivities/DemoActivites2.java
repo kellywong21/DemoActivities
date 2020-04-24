56ebc7acf102ed314b0985abf0ff7d25f7d152d7
18004885 <18004885@myrp.edu.sg>
2020-04-24 11:47:56 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoactivities/DemoActivites2.java
@@ -0,0 +1,18 @@
+package sg.edu.rp.c346.demoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DemoActivites2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_activites2);
+       
+    }
+}
